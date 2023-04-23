e1c11d91fc4db32c7a871cce72a819e6cc4a001f
JaxonAtBYUI <91147570+JaxonAtBYUI@users.noreply.github.com>
2023-04-22 22:48:40 -0600

    Added the code for "Hello World!"

---
+++ hello_world.cpp
@@ -0,0 +1,22 @@
+/****************************************************
+ * Program:
+ *    CS124 Review #1.10 , Hello World
+ * Author:
+ *    Jaxon Hamm
+ * Summary:
+ *    The classics of learning, make the computer
+ *    say Hello to the world!
+ *
+ *****************************************************/
+
+#include <iostream>
+using namespace std;
+
+/*****************************************************
+ * Display a message on the screen.
+ ******************************************************/
+int main()
+{
+    cout << "Hello World!\n";
+    return 0;
+}
